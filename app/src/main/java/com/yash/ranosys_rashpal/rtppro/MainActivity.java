bf5313e0925c5f60bb1c278009c9947bd777dbf2
ErRashpal <rashpal.singh+1@ranosys.com>
2017-02-08 19:24:31 +0530

    commit code

---
+++ app/src/main/java/com/yash/ranosys_rashpal/rtppro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yash.ranosys_rashpal.rtppro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
